286daee02522221b8e86ee94cd8c9746b3a913ba
jcwang666 <j2204620@gmail.com>
2017-03-07 19:56:35 +0800

    完成簡單登入程式

--- example01/Program.cs
+++ example01/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace example01
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.WriteLine("aAAAAAAAa");
+            Console.Write("使用者名稱:");
+            string userName = Console.ReadLine();
+            Console.Write("請輸入密碼");
+            string userPassword = Console.ReadLine();
+            Console.WriteLine("{0} 您好，登入成功 ,{1}", userName, userPassword);
+
+            Console.WriteLine("enter number1");
+            int number1 = 3;
+            Console.WriteLine("enter number2");
+            int number2 = 5;
+
+            int sum = number1 + number2;
+            Console.WriteLine("The Sum = {0}", sum);
         }
     }
 }
--- example02/Program.cs
+++ example02/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace example02
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.Write("aa");
         }
     }
 }
