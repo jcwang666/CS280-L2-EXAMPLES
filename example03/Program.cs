bdc73a93b837802cba686f6defa433406ba94143
jcwang666 <j2204620@gmail.com>
2017-03-07 20:45:56 +0800

    ex3 - string

---
+++ example03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example03
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string sql = "select * from table where id > 1000";
+
+            string xml =
+                @"<Pokemon>
+                    <NatioNO>#001</NationalNo>
+                     <Name>Bulbsaur</Name>
+                  </Pokeman>";
+            // string json =
+            //     @"Pokeman={}
+            //         "
+
+            Console.WriteLine(sql);
+            Console.WriteLine(xml);
+        }
+    }
+}
